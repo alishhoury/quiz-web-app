c5936ce750589f7e77f3a7103a071a3b6cad8504
ALi Shhoury <alishhoury78@gmail.com>
2025-04-21 21:37:53 +0300

    added general knowledge questions

---
+++ Quiz Master/scripts/quizData.js
@@ -0,0 +1,19 @@
+const quizzes ={
+    "General Knowledge": [
+        {
+            question: "What is the capital of France?",
+            answers: ["London", "Paris", "Berlin"],
+            correct: 1
+        },
+        {
+            question: "Which planet is known as the Red Planet?",
+            answers: ["Venus", "Mars", "Jupiter"],
+            correct: 1
+        },
+        {
+            question: "Who painted the Mona Lisa?",
+            answers: ["Van Gogh", "Da Vinci", "Picasso"],
+            correct: 1
+        }
+    ]
+}
